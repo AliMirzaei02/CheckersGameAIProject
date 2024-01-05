e5dfaf325757e6fc5bf610529904a9f11de0b2cf
Ali Mirzaei <93037774+AliMirzaei02@users.noreply.github.com>
2024-01-06 01:45:53 +0330

    set colors & textures

---
+++ checkers/constants.py
@@ -0,0 +1,17 @@
+import pygame
+
+WIDTH, HEIGHT = 700, 700
+ROWS, COLS = 8, 8
+SQUARE_SIZE = WIDTH//COLS
+
+# RGB colors
+RED = (255, 0, 0)
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+BLUE = (0, 0, 255)
+GREY = (128,128,128)
+BROWN = (64, 47, 29)
+LIGHT_BROWN = (193, 154, 107)
+
+CROWN = pygame.transform.scale(pygame.image.load('assets/crown.png'), (WIDTH/10, HEIGHT/10))
+TEXTURE = pygame.transform.scale(pygame.image.load('assets/texture.png'), (WIDTH/10, HEIGHT/10))
