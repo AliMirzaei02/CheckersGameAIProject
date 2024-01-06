528c9978020f85290cd2ea06d13419c91a0959bc
keivanmj <k1majnoughty@gmail.com>
2024-01-06 23:10:54 +0330

    created game.py class

--- minmax/algorithm.py
+++ minmax/algorithm.py
@@ -4,5 +4,28 @@ import pygame
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 
-def minMax(position, depth, max_player, game):
-    pass
+
+def Minmax(position, depth, max_player, game):
+    if depth == 0 or position.winner() != None:
+        return position.evaluate(), position
+
+    if max_player:
+        maxEval = float('-inf')
+        best_move = None
+        for move in get_all_moves(position, WHITE, game):
+            evaluation = minimax(move, depth - 1, False, game)[0]
+            maxEval = max(maxEval, evaluation)
+            if maxEval == evaluation:
+                best_move = move
+
+        return maxEval, best_move
+    else:
+        minEval = float('inf')
+        best_move = None
+        for move in get_all_moves(position, BLACK, game):
+            evaluation = minimax(move, depth - 1, True, game)[0]
+            minEval = min(minEval, evaluation)
+            if minEval == evaluation:
+                best_move = move
+
+        return minEval, best_move
